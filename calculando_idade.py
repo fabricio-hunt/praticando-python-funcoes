da278e2968060b01f44fc90e528f7ae61ff721c9
Fabricio Macedo <fabriciobaraunadev@gmail.com>
2025-11-27 14:50:16 -0400

    calculando idade

---
+++ calculando_idade.py
@@ -0,0 +1,11 @@
+def calculando_idade():
+    ano_nascimento = int(input('Digite o ano de nascimento: '))
+    ano_atual = int(input('Digite o ano atual: : '))
+    
+    calculando_idade = (ano_atual - ano_nascimento)
+    print(f"Sua idade é {calculando_idade}")
+    
+calculando_idade()
+    
+
+   
